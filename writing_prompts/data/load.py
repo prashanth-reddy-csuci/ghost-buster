a94dde93919207739669e6f1f22556c0c21d5609
vivek3141 <vivnps.verma@gmail.com>
2023-05-26 06:34:17 +0000

    Adding run functionality

--- writing_prompts/data/load.py
+++ writing_prompts/data/load.py
@@ -3,13 +3,13 @@ import numpy as np
 import os
 
 
-def generate_dataset(featurize, verbose=False):
+def generate_dataset(featurize, verbose=False, base_dir=""):
     data = []
     to_iter = tqdm.tqdm(range(1000)) if verbose else range(1000)
 
     for file_idx in to_iter:
         for source in ["human", "gpt"]:
-            data.append(featurize(f"data/{source}/{file_idx}.txt"))
+            data.append(featurize(f"{base_dir}data/{source}/{file_idx}.txt"))
 
     return np.array(data)
 
