792a946f483affb1a7b29932612423f77be278a5
vivek3141 <vivnps.verma@gmail.com>
2023-06-22 22:18:30 +0000

    Testing if this works

--- essay/generate_symbolic.py
+++
@@ -1,9 +0,0 @@
-from utils.symbolic import generate_symbolic_data
-from essay.data.load import generate_dataset
-
-generate_symbolic_data(
-    generate_dataset,
-    output_file="symbolic_data",
-    max_depth=3,
-    verbose=True
-)
